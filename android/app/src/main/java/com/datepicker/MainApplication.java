7cf42fa93f6ecaccae86b6671f127a512b6f8924
Martin Acosta <tinchoz49@gmail.com>
2018-02-06 20:08:26 -0300

    Added link dependency

--- android/app/src/main/java/com/datepicker/MainApplication.java
+++ android/app/src/main/java/com/datepicker/MainApplication.java
@@ -3,6 +3,7 @@ package com.datepicker;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.yutaro_mt.reactnative.datepicker.fix.DatePickerFixDialogPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new DatePickerFixDialogPackage()
       );
     }
 
